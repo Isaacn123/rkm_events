38e7af9e8d81c744b13ab07b6bebae9ea00dd4d6
Isaacn123 <nsambai72@gmail.com>
2025-08-25 12:13:57 +0300

    adding audio

--- frontend_admin copy/src/app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminDashboard from "@/components/mvpblocks";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminDashboard>{children}</AdminDashboard>;
-} 
--- frontend_admin copy/src/types/ckeditor__ckeditor5-react.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@ckeditor/ckeditor5-react' {
-  import * as React from 'react';
-  export const CKEditor: React.ComponentType<Record<string, unknown>>;
-}
